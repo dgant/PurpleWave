6fd8e4a63749815c7ef82cfcc40c8b464b48ad85
Dan Gant <no@email.com>
2017-02-03 13:16:25 -0500

    Build works, except for attacking! Very slow and does some goofy inefficient things, but it's well on its way. Scouting also works (albeit as of the previous commit)

--- src/Plans/GamePlans/Protoss/Proxy/ProtossRushWithProxyZealots.scala
+++ src/Plans/GamePlans/Protoss/Proxy/ProtossRushWithProxyZealots.scala
@@ -1,6 +1,6 @@
 package Plans.GamePlans.Protoss.Proxy
 
-import Plans.Generic.Compound.{PlanCompleteAllInParallel, PlanCompleteAllInSerial, PlanFulfillRequirements}
+import Plans.Generic.Compound.{PlanCompleteAllInParallel, PlanCompleteAllInSerial}
 import Plans.Generic.Macro.UnitAtLocation.PlanRequireUnitAtLocation
 import Plans.Generic.Macro.UnitCount.PlanCheckUnitCount
 import Plans.Generic.Macro.{PlanBuildBuilding, PlanTrainUnit}
@@ -9,23 +9,25 @@ import Strategies.PositionFinders.{PositionProxyGateway, PositionProxyPylon}
 import Strategies.UnitMatchers.UnitMatchWorker
 import bwapi.UnitType
 
-class ProtossRushWithProxyZealots extends PlanCompleteAllInParallel{
+class ProtossRushWithProxyZealots extends PlanCompleteAllInParallel {
   kids = List(
-    new PlanTrainUnit(UnitType.Protoss_Probe),
-    new PlanTrainUnit(UnitType.Protoss_Probe),
     new PlanCompleteAllInParallel { kids = List(
       new PlanCompleteAllInSerial { kids = List(
-        new PlanFulfillRequirements { kids = List(
-          new PlanCheckUnitCount { unitMatcher = UnitMatchWorker; minimum = 6 },
-          new PlanRequireUnitAtLocation(UnitMatchWorker, PositionProxyPylon, leashRange = 1024)
+        new PlanCompleteAllInSerial { kids = List(
+          new PlanCheckUnitCount { unitMatcher = UnitMatchWorker; minimum = 5 },
+          new PlanRequireUnitAtLocation(UnitMatchWorker, PositionProxyPylon, leashRange = 512)
         )},
         new PlanBuildBuilding(UnitType.Protoss_Pylon) { positionFinder = PositionProxyPylon },
         new PlanBuildBuilding(UnitType.Protoss_Gateway) { positionFinder = PositionProxyGateway },
         new PlanBuildBuilding(UnitType.Protoss_Gateway) { positionFinder = PositionProxyGateway },
         new PlanRequireEnemyBaseLocation
       )},
-      new PlanTrainUnit(UnitType.Protoss_Probe)
+      
     )},
+    new PlanTrainUnit(UnitType.Protoss_Probe),
+    new PlanTrainUnit(UnitType.Protoss_Probe),
+    new PlanTrainUnit(UnitType.Protoss_Probe),
+    new PlanTrainUnit(UnitType.Protoss_Probe),
     new PlanTrainUnit(UnitType.Protoss_Zealot),
     new PlanTrainUnit(UnitType.Protoss_Zealot),
     new PlanTrainUnit(UnitType.Protoss_Zealot),

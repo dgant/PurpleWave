26beee37cd3b2ffeaed5bbc4517ab85ca77a4655
Dan Gant <dgant@users.noreply.github.com>
2017-03-01 18:57:50 -0500

    Slight tweaks to dodging

--- src/Global/Combat/Commands/Dodge.scala
+++ src/Global/Combat/Commands/Dodge.scala
@@ -12,14 +12,17 @@ object Dodge extends Command{
     val closestEnemy = intent.battle.map(_.enemy.units.minBy(_.distanceSquared(unit)))
     
     val kitePositions =
-      (-4 to 4).flatten(dy =>
-        (-4 to 4).map(dx =>
+      (-5 to 5).flatten(dy =>
+        (-5 to 5).map(dx =>
           unit.tilePosition.add(dx, dy)
         ))
         .filter(With.geography.isWalkable)
     
     if (kitePositions.nonEmpty) {
-      With.commander.move(this, unit, kitePositions.map(_.toPosition).maxBy(closestEnemy.get.distanceSquared))
+      With.commander.move(this, unit,
+        kitePositions.map(_.toPosition)
+          .sortBy(position => position.distanceSquared(With.geography.home))
+          .maxBy(position => closestEnemy.get.distanceSquared(position).toInt/32))
     }
     else {
       With.logger.warn(unit.utype + " had nowhere to dodge near " + unit.tilePosition)

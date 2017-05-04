9907bc25ca7281193d1c643798c6b2df4ae8f79f
Dan Gant <dgant@users.noreply.github.com>
2017-05-04 11:49:40 -0400

    Restored preference for fighting at home

--- src/Information/Battles/EvaluateTactics.scala
+++ src/Information/Battles/EvaluateTactics.scala
@@ -2,6 +2,7 @@ package Information.Battles
 
 import Information.Battles.BattleTypes.Battle
 import Information.Battles.TacticsTypes.TacticsOptions
+import Lifecycle.With
 
 object EvaluateTactics {
   
@@ -14,6 +15,15 @@ object EvaluateTactics {
   }
   
   def evaluate(battle:Battle, tactics: TacticsOptions):Double = {
-    battle.estimation(tactics).map(estimation => estimation.costToUs - estimation.costToEnemy).sum
+    battle.estimation(tactics).map(estimation => estimation.costToUs - estimation.costToEnemy * desireToFight(battle)).sum
+  }
+  
+  def desireToFight(battle:Battle):Double = {
+    var output = 1.0
+    val zone = battle.focus.zone
+    if (zone.owner == With.self && zone.bases.nonEmpty) {
+      output *= 4.0 / Math.max(1.0, With.geography.ourBases.size)
+    }
+    output
   }
 }

b26b2606f6a3c060360c8fb7521d80229b6ce60a
Dan Gant <dgant@users.noreply.github.com>
2017-05-08 16:33:26 -0400

    Tweaked targeting to be less afraid to reach for an important unit, since we take potshots by default anyway

--- src/Micro/Behaviors/TargetingProfiles.scala
+++ src/Micro/Behaviors/TargetingProfiles.scala
@@ -5,7 +5,7 @@ import Micro.Heuristics.Targeting.TargetingProfile
 object TargetingProfiles {
   
   def default = new TargetingProfile(
-    preferInRange     = 5.0,
+    preferInRange     = 3.0,
     preferCombat      = 1.0,
     preferDps         = 1.5,
     preferDamageType  = 1.0,

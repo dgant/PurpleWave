74e783322ec46eb9bffa5a546019cb8de11dabc8
Dan Gant <dgant@users.noreply.github.com>
2017-06-16 11:31:09 -0400

    Movement profile tweaks.

--- src/Micro/Behaviors/MovementProfiles.scala
+++ src/Micro/Behaviors/MovementProfiles.scala
@@ -8,10 +8,10 @@ object MovementProfiles {
     preferDestination     = 1.00,
     preferOrigin          = 0.30,
     preferMobility        = 0.30,
-    preferThreatDistance  = 0.10,
-    preferTargetValue     = 1.00,
-    avoidTraffic          = 0.20,
-    avoidDamage           = 0.25)
+    preferThreatDistance  = 0.20,
+    preferTargetValue     = 0.75,
+    avoidTraffic          = 0.30,
+    avoidDamage           = 0.15)
   
   def approach = new MovementProfile(
     preferDestination     = 1.00,

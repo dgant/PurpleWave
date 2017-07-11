cd6016c7791161a6dbb6659f47fa8a1c3a3896ea
Dan Gant <dgant@users.noreply.github.com>
2017-07-11 14:09:02 -0400

    Tweaked targeting profiles

--- src/Micro/Behaviors/TargetingProfiles.scala
+++ src/Micro/Behaviors/TargetingProfiles.scala
@@ -8,10 +8,10 @@ object TargetingProfiles {
     preferInRange     = 3.0,
     preferValue       = 0.5,
     preferCombat      = 1.0,
-    preferDps         = 1.5,
+    preferDps         = 2.0,
     preferDamageType  = 1.0,
-    avoidPain         = 1.0,
+    avoidPain         = 2.0,
     avoidHealth       = 1.0,
-    avoidDistance     = 2.0,
+    avoidDistance     = 1.5,
     avoidDistraction  = 1.0)
 }

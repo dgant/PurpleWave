f1d8b4c9a6b1c5c574ba5520da2fecdcea736d8b
Dan Gant <dgant@users.noreply.github.com>
2017-07-11 20:47:18 -0400

    Fixed small compilation issue

--- src/Macro/Architecture/Heuristics/PlacementHeuristicCoversWorkers.scala
+++ src/Macro/Architecture/Heuristics/PlacementHeuristicCoversWorkers.scala
@@ -1,20 +1,16 @@
 package Macro.Architecture.Heuristics
 
 import Macro.Architecture.Blueprint
-import Mathematics.Heuristics.HeuristicMathMultiplicative
 import Mathematics.Points.Tile
 
 object PlacementHeuristicCoversWorkers extends PlacementHeuristic {
   
   override def evaluate(blueprint: Blueprint, candidate: Tile): Double = {
-    
-    if (blueprint.distancePixels.isEmpty) return HeuristicMathMultiplicative.default
-    
     candidate.zone.bases
       .map(
         _.harvestingArea.tiles.count(
           _.tileDistanceFast(candidate) * 32.0
-          <= blueprint.distancePixels.get))
+          <= blueprint.distancePixels))
       .sum
   }
 }

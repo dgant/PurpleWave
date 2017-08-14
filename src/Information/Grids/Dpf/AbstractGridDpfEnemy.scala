c96e631f820894ac2002a00d8fb54130296779e4
Dan Gant <dgant@users.noreply.github.com>
2017-08-14 17:00:58 -0400

    Eliminated DPF grids.

--- src/Information/Grids/Dpf/AbstractGridDpfEnemy.scala
+++
@@ -1,8 +0,0 @@
-package Information.Grids.Dpf
-
-import ProxyBwapi.UnitInfo.UnitInfo
-import Lifecycle.With
-
-abstract class AbstractGridDpfEnemy extends AbstractGridDpf {
-  override protected def getUnits: Iterable[UnitInfo] = With.units.enemy.filter(unit => unit.possiblyStillThere && unit.canAttack)
-}
--- src/Information/Grids/Grids.scala
+++ src/Information/Grids/Grids.scala
@@ -1,32 +1,23 @@
 package Information.Grids
 
 import Information.Grids.Construction._
-import Information.Grids.Dpf._
 import Information.Grids.Movement._
 import Information.Grids.Vision.{GridAltitudeBonus, GridEnemyDetection, GridFriendlyVision}
 
 class Grids {
   
-  val altitudeBonus             = new GridAltitudeBonus
-  val buildable                 = new GridBuildable
-  val buildableTerrain          = new GridBuildableTerrain
-  val chokepoints               = new GridChokepoint
-  val creep                     = new GridCreep
-  val friendlyVision            = new GridFriendlyVision
-  val enemyDetection            = new GridEnemyDetection
-  val dpfEnemyAirConcussive     = new GridDpfEnemyAirConcussive
-  val dpfEnemyAirExplosive      = new GridDpfEnemyAirExplosive
-  val dpfEnemyAirNormal         = new GridDpfEnemyAirNormal
-  val dpfEnemyGroundConcussive  = new GridDpfEnemyGroundConcussive
-  val dpfEnemyGroundExplosive   = new GridDpfEnemyGroundExplosive
-  val dpfEnemyGroundNormal      = new GridDpfEnemyGroundNormal
-  val dpfEnemyApproximate       = new GridDpfEnemyApproximate
-  val psi2Height                = new GridPsi2Height
-  val psi3Height                = new GridPsi3Height
-  val mobility                  = new GridMobility
-  val units                     = new GridUnits
-  val walkable                  = new GridWalkable
-  val walkableTerrain           = new GridWalkableTerrain
-  val walkableUnits             = new GridWalkableUnits
-  val dpfEnemy                  = new GridDpfEnemyToUnit
+  val altitudeBonus    = new GridAltitudeBonus
+  val buildable        = new GridBuildable
+  val buildableTerrain = new GridBuildableTerrain
+  val chokepoints      = new GridChokepoint
+  val creep            = new GridCreep
+  val friendlyVision   = new GridFriendlyVision
+  val enemyDetection   = new GridEnemyDetection
+  val psi2Height       = new GridPsi2Height
+  val psi3Height       = new GridPsi3Height
+  val mobility         = new GridMobility
+  val units            = new GridUnits
+  val walkable         = new GridWalkable
+  val walkableTerrain  = new GridWalkableTerrain
+  val walkableUnits    = new GridWalkableUnits
 }

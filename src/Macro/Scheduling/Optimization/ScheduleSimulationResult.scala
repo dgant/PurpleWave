a8d50ac5f748857f1cd74e2b58675eac362b5609
Dan Gant <dgant@users.noreply.github.com>
2017-08-14 17:05:53 -0400

    Eliminated build order simulation

--- src/Macro/Scheduling/Optimization/ScheduleSimulationResult.scala
+++
@@ -1,9 +0,0 @@
-package Macro.Scheduling.Optimization
-
-import Macro.Buildables.Buildable
-import Macro.Scheduling.BuildEvent
-
-class ScheduleSimulationResult(
-                                val suggestedEvents:Iterable[BuildEvent],
-                                val simulatedEvents:Iterable[BuildEvent],
-                                val unbuildable:Iterable[Buildable])
--- src/Macro/Scheduling/Optimization/TryBuildingResult.scala
+++
@@ -1,9 +0,0 @@
-package Macro.Scheduling.Optimization
-
-import Macro.Buildables.Buildable
-import Macro.Scheduling.BuildEvent
-
-class TryBuildingResult(
-  val buildEvent:Option[BuildEvent],
-  val unmetPrerequisites:Iterable[Buildable] = Vector.empty,
-  val exceededSearchDepth:Boolean = false)

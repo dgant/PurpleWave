768d4b18007650fb36f22a5b135cb3e9ae73c9a4
Dan Gant <dgant@users.noreply.github.com>
2017-08-15 16:54:46 -0400

    Added more detailed visualization for battle estimations.

--- src/Debugging/Visualizations/Views/Battles/ShowBattleDetails.scala
+++ src/Debugging/Visualizations/Views/Battles/ShowBattleDetails.scala
@@ -5,8 +5,10 @@ import Debugging.Visualizations.Rendering.{DrawMap, DrawScreen}
 import Debugging.Visualizations.Views.View
 import Information.Battles.Estimations.Estimation
 import Information.Battles.Estimations.Simulation.Simulacrum
-import Information.Battles.Types.{Battle, Team}
+import Information.Battles.Types.Battle
 import Lifecycle.With
+import ProxyBwapi.UnitInfo.UnitInfo
+import bwapi.Color
 
 object ShowBattleDetails extends View {
   
@@ -14,19 +16,19 @@ object ShowBattleDetails extends View {
     ShowBattles.localBattle.foreach(battle => {
       renderBattleScreen(
         battle,
-        battle.estimationSimulationAttack,
-        5,
-        5 * With.visualization.lineHeightSmall)
+        battle.estimationSimulationAttack)
     })
   }
   
-  def renderBattleScreen(battle: Battle, estimation: Estimation, x: Int, y: Int) {
+  def renderBattleScreen(battle: Battle, estimation: Estimation) {
+    val x = 5
+    val y = 7 * With.visualization.lineHeightSmall
     val table = Vector(
       Vector[String]("", With.self.name, With.enemy.name),
       Vector[String]("", if (estimation.weSurvive) "Survives" else "", if (estimation.enemySurvives) "Survives" else ""),
-      Vector[String]("Seconds", "" + estimation.frames / 24, ""),
-      Vector[String]("Value", "" + estimation.costToEnemy.toInt, "" + estimation.costToUs.toInt),
-      Vector[String]("Participants", "" + estimation.totalUnitsUs, "" + estimation.totalUnitsEnemy),
+      Vector[String]("Seconds",       "" + estimation.frames / 24,        ""),
+      Vector[String]("Value",         "" + estimation.costToEnemy.toInt,  "" + estimation.costToUs.toInt),
+      Vector[String]("Participants",  "" + estimation.totalUnitsUs,       "" + estimation.totalUnitsEnemy),
       Vector[String]("Survivors",
         survivorPercentage(estimation.deathsUs,    estimation.totalUnitsUs),
         survivorPercentage(estimation.deathsEnemy, estimation.totalUnitsEnemy)),
@@ -34,13 +36,56 @@ object ShowBattleDetails extends View {
     DrawScreen.table(x, y, table)
     
     val y2 = y + (table.length + 4) * With.visualization.lineHeightSmall
-    val y3 = y2 + With.visualization.lineHeightSmall
-    With.game.drawTextScreen(x, y2, describeTeam(battle.us))
-    With.game.drawTextScreen(x, y3, describeTeam(battle.enemy))
+    DrawScreen.column(5, y2,
+      Vector(
+      "Participants",
+      describeTeam(battle.us.units),
+      describeTeam(battle.enemy.units),
+      "",
+      "Survivors",
+      describeTeam(estimation.reportCards.filter(_._1.isFriendly) .filterNot(_._2.dead).keys),
+      describeTeam(estimation.reportCards.filter(_._1.isEnemy)    .filterNot(_._2.dead).keys),
+      "",
+      "Deaths",
+      describeTeam(estimation.reportCards.filter(_._1.isFriendly) .filter(_._2.dead).keys),
+      describeTeam(estimation.reportCards.filter(_._1.isEnemy)    .filter(_._2.dead).keys)
+    ))
+    
+    val x2 = 200
+    val valueUsInitial    = battle.us.units.map(_.subjectiveValue).sum
+    val valueEnemyInitial = battle.us.units.map(_.subjectiveValue).sum
+    val valueUsLost       = estimation.costToUs.toInt
+    val valueEnemyLost    = estimation.costToEnemy.toInt
+    val valueUsKept       = Math.max(0, valueUsInitial - valueUsLost)
+    val valueEnemyKept    = Math.max(0, valueEnemyInitial - valueEnemyLost)
+    val baseline          = 200 / Array(1, valueUsInitial, valueEnemyInitial).max
+    val dxUsInitial       = valueUsInitial    / baseline
+    val dxEnemyInitial    = valueEnemyInitial / baseline
+    val dxUsLost          = valueUsLost       / baseline
+    val dxEnemyLost       = valueEnemyLost    / baseline
+    val dxUsKept          = valueUsKept       / baseline
+    val dxEnemyKept       = valueEnemyKept    / baseline
+    var nextY             = y
+    val boxHeight         = With.visualization.lineHeightSmall
+    val margin            = 2
+    
+    val colorUs     = With.self.colorMedium
+    val colorEnemy  = With.enemy.colorMedium
+    drawBar(x2, nextY, dxUsInitial,     boxHeight, colorUs,     "Start (Us)")     ; nextY += boxHeight
+    drawBar(x2, nextY, dxEnemyInitial,  boxHeight, colorEnemy,  "Start (Enemy)")  ; nextY += boxHeight + margin
+    drawBar(x2, nextY, dxUsKept,        boxHeight, colorUs,     "Kept (Us)")      ; nextY += boxHeight
+    drawBar(x2, nextY, dxEnemyKept,     boxHeight, colorEnemy,  "Kept (Enemy)")   ; nextY += boxHeight + margin
+    drawBar(x2, nextY, dxUsLost,        boxHeight, colorUs,     "Lost (Us)")      ; nextY += boxHeight
+    drawBar(x2, nextY, dxEnemyLost,     boxHeight, colorEnemy,  "Lost (Enemy)")
   }
   
-  def describeTeam(team: Team): String = {
-    team.units.groupBy(_.unitClass).toVector.sortBy( - _._2.size).map(p => p._2.size + " " + p._1).mkString(", ")
+  def drawBar(x: Int, y: Int, dx: Int, dy: Int, color: Color, label: String) {
+    With.game.drawBoxScreen(x, y, x+dx, y+dy, color)
+    With.game.drawTextScreen(x + 1, y + 1, label)
+  }
+  
+  def describeTeam(units: Iterable[UnitInfo]): String = {
+    units.groupBy(_.unitClass).toVector.sortBy( - _._2.size).map(p => p._2.size + " " + p._1).mkString(", ")
   }
   
   def survivorPercentage(deaths: Double, total: Double): String = {

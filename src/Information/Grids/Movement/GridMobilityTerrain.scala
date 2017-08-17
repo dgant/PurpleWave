a97073105636c6fd3e7787a6f5046c2eb77030c8
Dan Gant <dgant@users.noreply.github.com>
2017-08-17 17:56:47 -0400

    Fixed mobility calculation

--- src/Information/Grids/Movement/GridMobilityTerrain.scala
+++ src/Information/Grids/Movement/GridMobilityTerrain.scala
@@ -2,21 +2,56 @@ package Information.Grids.Movement
 
 import Information.Grids.ArrayTypes.AbstractGridArray
 import Lifecycle.With
-import Mathematics.Points.Tile
 
 class GridMobilityTerrain extends AbstractGridArray[Int] {
   
   override def onInitialization() {
+    
+    With.grids.walkableTerrain.initialize()
+    
     var mobility    = 0
-    var horizon     = (0 until length).filterNot(With.grids.walkableTerrain.get).toArray
+    val horizon     = new Array[Boolean](length)
+    val nextHorizon = new Array[Boolean](length)
     val explored    = new Array[Boolean](length)
     
-    while (horizon.nonEmpty) {
-      if (mobility > 0) {
-        horizon.foreach(set(_, mobility))
+    var i = 0
+    while (i < length) {
+      horizon(i) = ! With.grids.walkableTerrain.get(i)
+      i += 1
+    }
+    
+    @inline
+    def explore(nextIndex: Int) {
+      if (nextIndex >= 0 && nextIndex < length) {
+        nextHorizon(nextIndex) = ! explored(nextIndex) && ! horizon(nextIndex)
       }
-      horizon.foreach(explored(_) = true)
-      horizon = horizon.flatMap(new Tile(_).adjacent4).filterNot(explored.contains).map(_.i)
+    }
+  
+    var horizonSize = 1
+    while (horizonSize > 0) {
+      
+      i = 0
+      horizonSize = 0
+      while (i < length) {
+        if (horizon(i)) {
+          horizonSize += 1
+          values(i)   = mobility
+          explored(i) = true
+          explore(i - 1)
+          explore(i + 1)
+          explore(i - With.mapTileWidth)
+          explore(i + With.mapTileWidth)
+        }
+        i += 1
+      }
+      
+      i = 0
+      while (i < length) {
+        horizon(i) = nextHorizon(i)
+        nextHorizon(i) = false
+        i += 1
+      }
+  
       mobility += 1
     }
   }

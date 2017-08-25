5edec58603948b60a9885a62aaa1284d52e71c91
Dan Gant <dgant@users.noreply.github.com>
2017-08-25 19:19:07 -0400

    Tweaked desire algorithm a bit more. Extracted analysis into its own class for debugging.

---
+++ src/Information/Battles/Types/Analysis.scala
@@ -0,0 +1,48 @@
+package Information.Battles.Types
+
+import Lifecycle.With
+import Mathematics.PurpleMath
+import ProxyBwapi.UnitInfo.UnitInfo
+
+class Analysis(battle: Battle) {
+  val nearestBaseOurs   = if (With.geography.ourBases.isEmpty)    With.geography.home.pixelCenter                     else With.geography.ourBases  .map(_.heart.pixelCenter).minBy(_.groundPixels(battle.focus))
+  val nearestBaseEnemy  = if (With.geography.enemyBases.isEmpty)  With.intelligence.mostBaselikeEnemyTile.pixelCenter else With.geography.enemyBases.map(_.heart.pixelCenter).minBy(_.groundPixels(battle.focus))
+  val urgencyOurs       = battle.focus.pixelDistanceFast(nearestBaseEnemy)
+  val urgencyEnemy      = battle.focus.pixelDistanceFast(nearestBaseOurs)
+  val fighters          = battle.us.units.filter(_.canAttack)
+  val aggressionDesire  = With.blackboard.aggressionRatio
+  val flexibilityOurs   = meanFlexibility(battle.us.units)
+  val flexibilityEnemy  = meanFlexibility(battle.enemy.units)
+  val flexibilityRatio  = flexibilityOurs / flexibilityEnemy
+  val urgencyRatio      = urgencyOurs / urgencyEnemy
+  val chokiness         = if (battle.us.centroid.zone == battle.enemy.centroid.zone) 1.0 else 0.0
+  val economyRatio      = With.geography.ourBases.size.toDouble / With.geography.enemyBases.size
+  val hysteresisVoters  = battle.us.units.filter(unit => unit.canMove && unit.friendly.exists(_.agent.canFight))
+  val hysteresisEngaged = hysteresisVoters.filter(_.friendly.exists(_.agent.shouldEngage))
+  val hysteresisRatio   = if (hysteresisVoters.isEmpty) 0.0 else hysteresisEngaged.size.toDouble / hysteresisVoters.size
+  val hysteresis        = 2.0 * hysteresisRatio
+  val flexibilityDesire = PurpleMath.clamp(flexibilityRatio,  0.9, 1.2)
+  val urgencyDesire     = PurpleMath.clamp(urgencyRatio,      0.9, 1.5)
+  val chokinessDesire   = PurpleMath.clamp(chokiness,         0.8, 1.0)
+  val economyDesire     = PurpleMath.clamp(economyRatio,      0.9, 1.5)
+  val hysteresisDesire  = PurpleMath.clamp(hysteresis,        0.9, 2.0)
+  val bonusDesire       = aggressionDesire * flexibilityDesire * urgencyDesire * chokinessDesire * economyDesire * hysteresisDesire
+  val estimationAttack  = battle.estimationSimulationAttack
+  val estimationRetreat = battle.estimationSimulationRetreat
+  val attackGains       = estimationAttack.costToEnemy  + estimationRetreat.costToUs
+  val attackLosses      = estimationAttack.costToUs     + estimationRetreat.costToEnemy
+  val desire            = bonusDesire * attackGains - attackLosses
+  
+  private def meanFlexibility(units: Seq[UnitInfo]): Double = {
+    val fighters = units.filter(_.damageOnHitMax > 0)
+    val totalFlexibility = fighters.map(fighter => flexibility(fighter) * fighter.subjectiveValue).sum
+    val denominator = Math.max(fighters.map(_.subjectiveValue).sum, 1.0)
+    totalFlexibility / denominator
+  }
+    
+    private def flexibility(unit: UnitInfo): Double = {
+    val rangeFlexibility = unit.pixelRangeMax
+    val speedFlexibility = unit.topSpeed * 24.0 * (if (unit.flying) 2.0 else 1.0)
+    Math.max(rangeFlexibility, speedFlexibility)
+  }
+}
--- src/Information/Battles/Types/Battle.scala
+++ src/Information/Battles/Types/Battle.scala
@@ -2,11 +2,8 @@ package Information.Battles.Types
 
 import Information.Battles.BattleUpdater
 import Information.Battles.Estimations.Estimation
-import Lifecycle.With
 import Mathematics.Points.Pixel
-import Mathematics.PurpleMath
 import ProxyBwapi.UnitInfo.UnitInfo
-import Utilities.EnrichPixel.EnrichedPixelCollection
 
 class Battle(
   val us    : Team,
@@ -14,6 +11,15 @@ class Battle(
   
   us.battle     = this
   enemy.battle  = this
+  
+  //////////////
+  // Features //
+  //////////////
+  
+  def teams: Vector[Team] = Vector(us, enemy)
+  def teamOf(unit: UnitInfo): Team = if (unit.isFriendly) us else enemy
+  def focus: Pixel = teams.map(_.vanguard).centroid
+  def happening: Boolean = teams.forall(_.units.nonEmpty) && teams.exists(_.units.exists(_.canAttack))
   
   /////////////////
   // Estimations //
@@ -24,65 +30,12 @@ class Battle(
   lazy val estimationSimulationAttack   : Estimation  = BattleUpdater.estimateSimulation(this, weAttack = true)
   lazy val estimationSimulationRetreat  : Estimation  = BattleUpdater.estimateSimulation(this, weAttack = false)
   
-  //////////////
-  // Features //
-  //////////////
-  
-  def teams: Vector[Team] = Vector(us, enemy)
-  
-  def teamOf(unit: UnitInfo): Team = if (unit.isFriendly) us else enemy
-  
-  def focus: Pixel = teams.map(_.vanguard).centroid
-  
-  def happening: Boolean = teams.forall(_.units.nonEmpty) && teams.exists(_.units.exists(_.canAttack))
-  
   ///////////////
   // Judgement //
   ///////////////
   
-  lazy val desire: Double = {
-    val nearestBaseOurs   = if (With.geography.ourBases.isEmpty)    With.geography.home.pixelCenter                     else With.geography.ourBases  .map(_.heart.pixelCenter).minBy(_.groundPixels(focus))
-    val nearestBaseEnemy  = if (With.geography.enemyBases.isEmpty)  With.intelligence.mostBaselikeEnemyTile.pixelCenter else With.geography.enemyBases.map(_.heart.pixelCenter).minBy(_.groundPixels(focus))
-    val urgencyOurs       = focus.pixelDistanceFast(nearestBaseEnemy)
-    val urgencyEnemy      = focus.pixelDistanceFast(nearestBaseOurs)
-    val fighters          = us.units.filter(_.canAttack)
-    val aggressionDesire  = With.blackboard.aggressionRatio
-    val flexibilityOurs   = meanFlexibility(us.units)
-    val flexibilityEnemy  = meanFlexibility(enemy.units)
-    val flexibilityRatio  = flexibilityOurs / flexibilityEnemy
-    val urgencyRatio      = urgencyOurs / urgencyEnemy
-    val chokiness         = if (us.centroid.zone == enemy.centroid.zone) 1.0 else 0.0
-    val economyRatio      = With.geography.ourBases.size.toDouble / With.geography.enemyBases.size
-    val hysteresisVoters  = us.units.filter(unit => unit.canMove && unit.friendly.exists(_.agent.canFight))
-    val hysteresisEngaged = hysteresisVoters.filter(_.friendly.exists(_.agent.shouldEngage))
-    val hysteresisRatio   = if (hysteresisVoters.isEmpty) 0.0 else hysteresisEngaged.size.toDouble / hysteresisVoters.size
-    val hysteresis        = 2.0 * hysteresisRatio
-    val flexibilityDesire = PurpleMath.clamp(flexibilityRatio,  0.9, 1.2)
-    val urgencyDesire     = PurpleMath.clamp(urgencyRatio,      0.9, 1.5)
-    val chokinessDesire   = PurpleMath.clamp(chokiness,         0.8, 1.0)
-    val economyDesire     = PurpleMath.clamp(economyRatio,      0.9, 1.5)
-    val hysteresisDesire  = PurpleMath.clamp(hysteresis,        0.9, 2.0)
-    val bonusDesire       = aggressionDesire * flexibilityDesire * urgencyDesire * chokinessDesire * economyDesire * hysteresisDesire
-    val estimationAttack  = estimationSimulationAttack
-    val estimationRetreat = estimationSimulationRetreat
-    val attackGains       = estimationAttack.costToEnemy  + estimationRetreat.costToUs
-    val attackLosses      = estimationAttack.costToUs     + estimationRetreat.costToEnemy
-    val output            = bonusDesire * attackGains - attackLosses
-    output
-  }
-  
-  private def meanFlexibility(units: Seq[UnitInfo]): Double = {
-    val fighters = units.filter(_.damageOnHitMax > 0)
-    val totalFlexibility = fighters.map(fighter => flexibility(fighter) * fighter.subjectiveValue).sum
-    val denominator = Math.max(fighters.map(_.subjectiveValue).sum, 1.0)
-    totalFlexibility / denominator
-  }
-  
-  private def flexibility(unit: UnitInfo): Double = {
-    val rangeFlexibility = unit.pixelRangeMax
-    val speedFlexibility = unit.topSpeed * 24.0 * (if (unit.flying) 2.0 else 1.0)
-    Math.max(rangeFlexibility, speedFlexibility)
-  }
+  lazy val analysis = new Analysis(this)
+  lazy val desire: Double = analysis.desire
   
   lazy val globalSafeToAttack: Boolean = {
     estimationAbstractOffense.weSurvive || estimationAbstractOffense.enemyDies || estimationAbstractOffense.netValue > 0

35eb6d9021fa71566648e1cc98cd0c6f714d8504
Dan Gant <dgant@users.noreply.github.com>
2017-08-26 17:08:29 -0400

    Further base refactoring

--- src/Information/Geography/Calculations/BaseUpdater.scala
+++ src/Information/Geography/Calculations/BaseUpdater.scala
@@ -19,24 +19,23 @@ object BaseUpdater {
     base.owner = base.townHall.map(_.player).getOrElse(With.neutral)
     
     // Assume ownership of occupied base we haven't seen lately
-    if (base.owner.isNeutral && base.lastScoutedFrame < With.framesSince(Protoss.Nexus.buildFrames)) {
+    if (base.owner.isNeutral && With.framesSince(base.lastScoutedFrame) > Protoss.Nexus.buildFrames) {
       base.zone.units
-        .find(unit => unit.isEnemy && ! unit.flying && unit.unitClass.isBuilding && unit.zone == base.zone)
+        .find(unit => unit.isEnemy && ! unit.flying && unit.unitClass.isBuilding)
         .foreach(enemyBuilding => base.owner = enemyBuilding.player)
     }
     
     // Assume ownership of unscouted main from natural
     if (base.owner.isNeutral && ! base.scouted) {
-      val natural = With.geography.bases.find(_.isNaturalOf.contains(base))
-      natural.filterNot(_.owner.isFriendly).foreach(someNatural => base.owner = someNatural.owner)
+      base.natural.filter(_.owner.isEnemy).foreach(natural => base.owner = natural.owner)
     }
   }
   
   private def updateAssets(base: Base) {
     base.units          = base.zone.units.filter(_.base.contains(base))
     base.minerals       = base.units.filter(u => u.mineralsLeft > 0 && ! u.isMineralBlocker)
-    base.gas            = base.units.filter(u => u.unitClass.isGas)
-    base.workers        = base.units.filter(u => u.unitClass.isWorker)
+    base.gas            = base.units.filter(_.unitClass.isGas)
+    base.workers        = base.units.filter(_.unitClass.isWorker)
     base.mineralsLeft   = base.minerals.toSeq.map(_.mineralsLeft).sum
     base.gasLeft        = base.gas.toSeq.map(_.gasLeft).sum
     base.harvestingArea = (Vector(base.townHallArea) ++ (base.minerals.filter(_.mineralsLeft > With.configuration.blockerMineralThreshold) ++ base.gas).map(_.tileArea)).boundary

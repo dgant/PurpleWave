45369e67cf638a5f413b57da9fe324d896562bbf
Dan Gant <dgant@users.noreply.github.com>
2017-09-13 20:06:57 -0400

    2-Base Carrier build tweaks

--- src/Planning/Plans/Protoss/GamePlans/Standard/PvT/PvT2BaseCarriers.scala
+++ src/Planning/Plans/Protoss/GamePlans/Standard/PvT/PvT2BaseCarriers.scala
@@ -3,10 +3,10 @@ package Planning.Plans.Protoss.GamePlans.Standard.PvT
 import Macro.BuildRequests.{RequestAtLeast, RequestUpgrade}
 import Planning.Plan
 import Planning.Plans.Army.{ConsiderAttacking, DefendZones}
-import Planning.Plans.Compound.{FlipIf, If, Parallel, Trigger}
+import Planning.Plans.Compound.{FlipIf, If, Parallel}
 import Planning.Plans.GamePlans.Mode
-import Planning.Plans.Information.{Employing, Never}
 import Planning.Plans.Information.Reactive.EnemyBio
+import Planning.Plans.Information.{Employing, Never}
 import Planning.Plans.Macro.Automatic.{RequireSufficientSupply, TrainContinuously, TrainWorkersContinuously}
 import Planning.Plans.Macro.BuildOrders.{Build, BuildOrder}
 import Planning.Plans.Macro.Expanding.{BuildCannonsAtExpansions, RequireMiningBases}
@@ -28,13 +28,13 @@ class PvT2BaseCarriers extends Mode {
     new Require2BaseTech,
     new RequireSufficientSupply,
     new TrainWorkersContinuously,
+    new If(
+      new UnitsAtLeast(1, Protoss.Carrier, complete = true),
+      new Build(RequestUpgrade(Protoss.CarrierCapacity))),
     new FlipIf(
       new UnitsAtLeast(12, Protoss.Dragoon),
       new TrainContinuously(Protoss.Dragoon),
       new TrainContinuously(Protoss.Carrier)),
-    new If(
-      new UnitsAtLeast(1, Protoss.Carrier, complete = true),
-      new Build(RequestUpgrade(Protoss.CarrierCapacity))),
     new BuildOrder(
       RequestAtLeast(2, Protoss.Gateway),
       RequestAtLeast(1, Protoss.Stargate)),
@@ -61,13 +61,9 @@ class PvT2BaseCarriers extends Mode {
     new UpgradeContinuously(Protoss.AirArmor),
     new UpgradeContinuously(Protoss.AirDamage),
     new ScoutExpansionsAt(100),
-    new Trigger(
-      new UnitsAtLeast(24, Protoss.Interceptor),
-      initialBefore = new DefendZones,
-      initialAfter = new Parallel(
-        new ConsiderAttacking { attack.attackers.get.unitMatcher.set(Protoss.Carrier) },
-        new DefendZones,
-        new ConsiderAttacking))
+    new ConsiderAttacking { attack.attackers.get.unitMatcher.set(Protoss.Carrier) },
+    new DefendZones,
+    new ConsiderAttacking
   ))
 }
 

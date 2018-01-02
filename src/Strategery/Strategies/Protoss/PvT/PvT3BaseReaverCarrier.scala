3cea847f233507bfc3f76a5554228fcc7b9e2770
Dan Gant <dgant@users.noreply.github.com>
2018-01-02 00:30:56 -0500

    Added missing files

---
+++ src/Strategery/Strategies/Protoss/PvT/PvT3BaseReaverCarrier.scala
@@ -0,0 +1,10 @@
+package Strategery.Strategies.Protoss.PvT
+
+import Strategery.Strategies.Strategy
+import bwapi.Race
+
+object PvT3BaseReaverCarrier extends Strategy {
+  
+  override def ourRaces    : Iterable[Race] = Vector(Race.Protoss)
+  override def enemyRaces  : Iterable[Race] = Vector(Race.Terran)
+}

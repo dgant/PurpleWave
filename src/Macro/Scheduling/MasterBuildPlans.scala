32005d0b867a56b942a0c61e4bdad3d2a23e5028
Dan Gant <dgant@users.noreply.github.com>
2019-09-09 22:18:18 -0400

    Reverting MasterBuildPlans changes on suspicion of causing multiple Pylon builds

--- src/Macro/Scheduling/MasterBuildPlans.scala
+++ src/Macro/Scheduling/MasterBuildPlans.scala
@@ -2,58 +2,72 @@ package Macro.Scheduling
 
 import Lifecycle.With
 import Macro.Buildables.Buildable
+import Performance.Cache
 import Planning.Plan
 import Planning.Plans.Macro.Build._
 import Planning.Plans.Macro.BuildOrders.FollowBuildOrder
 
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 class MasterBuildPlans {
-  
+
   private val maxToFollow = 200
 
-  private var childrenPlans: mutable.ArrayBuffer[ProductionPlan] = mutable.ArrayBuffer.empty
-  
-  def getChildren: Seq[Plan] = childrenPlans
+  private val plans = new mutable.HashMap[Buildable, ListBuffer[Plan]]
+  private var queue: Iterable[Buildable] = Vector.empty
 
-  var lastUpdateFrame: Int = -1
-  def update(invoker: FollowBuildOrder): Unit = {
-    if (With.frame == lastUpdateFrame) return
-    lastUpdateFrame = With.frame
-
-    // 1. Release completed plans
-    // 2. Map current buildables to available plans; produce new plans as needed
-    // 3. Replace the queue
-
-    val newPlans = new mutable.ArrayBuffer[ProductionPlan](childrenPlans.size)
-    val livingPlans = new mutable.ListBuffer[ProductionPlan]
-    var i = 0
-    childrenPlans.foreach(plan => {
-      if (plan.isComplete) {
-        With.recruiter.release(plan)
-      }
-      else {
-        livingPlans += plan
-      }
+  def getChildren: Iterable[Plan] = getChildrenCache()
+  private val getChildrenCache = new Cache(() => getChildrenRecalculate)
+  private def getChildrenRecalculate: Iterable[Plan] = {
+    val indexByBuild = new mutable.HashMap[Buildable, Int]
+    plans.keys.foreach(build => indexByBuild.put(build, 0))
+    queue.map(build => {
+      val output = plans(build)(indexByBuild(build))
+      indexByBuild(build) += 1
+      output
     })
-
-    With.scheduler.queue.take(maxToFollow).foreach(buildable => {
-      var plan: Option[ProductionPlan] = None
-      i = 0
-      while (i < livingPlans.length) {
-        if (livingPlans(i).buildable == buildable) {
-          plan = Some(livingPlans.remove(i))
-          i = livingPlans.length
-        }
-        i += 1
-      }
-      plan = plan.orElse(Some(makeBuildPlan(buildable)))
-      newPlans += plan.get
-    })
-    childrenPlans = newPlans
   }
-  
-  private def makeBuildPlan(buildable: Buildable): ProductionPlan = {
+
+  def update(invoker: FollowBuildOrder) {
+
+    // Remove complete plans
+    plans.values.foreach(plans => {
+      var i = 0
+      while (i < plans.size) {
+        val plan = plans(i)
+        if (plan.isComplete) {
+          With.recruiter.release(plan)
+          plans.remove(i)
+        }
+        else {
+          i += 1
+        }
+      }})
+
+    // Add plans to match number of builds we need
+    queue = With.scheduler.queue.take(maxToFollow)
+
+    val buildsNeeded =
+      queue
+        .groupBy(buildable => buildable)
+        .map(buildable => (
+          buildable._1,
+          buildable._2.size))
+
+    buildsNeeded.keys.foreach(build => {
+      if ( ! plans.contains(build)) {
+        plans.put(build, new ListBuffer[Plan])
+      }
+      while (plans(build).size < buildsNeeded(build)) {
+        plans(build).append(buildPlan(build))
+      }
+    })
+
+    getChildrenCache.invalidate()
+  }
+
+  private def buildPlan(buildable: Buildable): Plan = {
     if (buildable.unitOption.nonEmpty) {
       val unitClass = buildable.unitOption.get
       if (unitClass.isAddon) {
@@ -73,7 +87,7 @@ class MasterBuildPlans {
     if (buildable.upgradeOption.nonEmpty) {
       return new ResearchUpgrade(buildable.upgradeOption.get, buildable.upgradeLevel)
     }
-    
+
     throw new Exception("Tried to build a Buildable that doesn't specify any unit, tech, or research")
   }
 }

94cf37b6684596e54175007aec075f706e5220b1
Dan Gant <dgant@users.noreply.github.com>
2020-01-12 18:42:47 -0500

    First functioning gatherer!

--- src/Planning/Plans/Macro/Automatic/Gather.scala
+++ src/Planning/Plans/Macro/Automatic/Gather.scala
@@ -3,6 +3,8 @@ package Planning.Plans.Macro.Automatic
 import Information.Geography.Types.Base
 import Lifecycle.With
 import Mathematics.PurpleMath
+import Micro.Agency.Intention
+import Performance.Cache
 import Planning.Plan
 import Planning.ResourceLocks.LockUnits
 import Planning.UnitCounters.UnitCountEverything
@@ -25,7 +27,7 @@ class Gather extends Plan {
 
   val resourceByWorker = new mutable.HashMap[FriendlyUnitInfo, UnitInfo]
   val workersByResource = new mutable.HashMap[UnitInfo, mutable.Set[FriendlyUnitInfo]]
-  def assignWorker(worker: FriendlyUnitInfo, resource: FriendlyUnitInfo): Unit = {
+  def assignWorker(worker: FriendlyUnitInfo, resource: UnitInfo): Unit = {
     unassignWorker(worker)
     resourceByWorker(worker) = resource
     workersByResource(resource) += worker
@@ -65,6 +67,30 @@ class Gather extends Plan {
   val kDistanceMining: Int = 32 * 12
   val lightYear: Double = 1e10
 
+  lazy val baseCosts: Map[(Base, Base), Cache[Double]] = With.geography.bases
+    .flatMap(baseA => With.geography.bases.map(baseB => (baseA, baseB)))
+    .map(basePair => (basePair, new Cache[Double](() =>
+      if (basePair._1 == basePair._2) 0.0
+      else if (basePair._1.hashCode() > basePair._2.hashCode()) baseCosts((basePair._2, basePair._1)).apply()
+      else basePair._1.heart.groundPixels(basePair._2.heart) // TODO: Account for unsafe travel
+    )))
+    .toMap
+
+  lazy val depotToResourceDistances = new Cache(() => workersByResource
+    .keysIterator
+    .map(resource => (
+      resource,
+      ByOption
+        .min(
+          With.geography
+            .ourBases
+            .filter(_.townHall.isDefined)
+            .map(base =>
+              if (resource.base.contains(base)) base.townHall.get.pixelDistanceEdge(resource)
+              else base.townHallTile.groundPixels(resource.tileTopLeft)))
+        .getOrElse(lightYear)))
+    .toMap)
+
   override def onUpdate() {
     workerLock.acquire(this)
     workers = workerLock.units
@@ -84,28 +110,7 @@ class Gather extends Plan {
 
     // Count gas workers
     var gasWorkersNow = resourceByWorker.count(_._2.unitClass.isGas)
-
-    // Identify base-to-base transfer costs, considering distance and threats
-    val baseCosts = new mutable.HashMap[(Base, Base), Double]
-    With.geography.ourBases.foreach(base0 =>
-      With.geography.ourBases.foreach(base1 => {
-        val cost = base0.heart.groundPixels(base1.heart)
-        // TODO: Account for unsafe travel
-        baseCosts((base0, base1)) = cost
-        baseCosts((base1, base0)) = cost
-      }))
-
     // Map resources to bases and measure distance
-    // TODO: Save calculation
-    val depotToResourceDistances = workersByResource
-      .keys
-      .map(resource => (
-        resource,
-        ByOption
-          .min(With.geography.ourBases.filter(_.townHall.isDefined).map(base =>
-            if (resource.base.contains(base)) base.townHall.get.pixelDistanceEdge(resource) else base.townHallTile.groundPixels(resource.tileTopLeft)))
-          .getOrElse(lightYear)))
-      .toMap
 
     val totalMineralPatches = With.geography.ourBases.view.map(_.minerals.size).sum
     val totalGasPumps = With.geography.ourBases.view.map(_.gas.count(_.isOurs)).sum
@@ -145,17 +150,15 @@ class Gather extends Plan {
     val workersToUpdate = workers.toVector.sortBy(worker =>
       // TODO: Make sure this has the correct sign!
       if (needMoreGasWorkers) {
-        if (newResources.nonEmpty) {
-          newGasDistance(worker)
-        } else {
-          minGasDistance.getOrElse(worker, None).getOrElse(Double.PositiveInfinity)
-        }
+        newGasDistance(worker)
+          .getOrElse(minGasDistance.getOrElse(worker, None)
+          .getOrElse(Double.PositiveInfinity))
       } else {
-        worker.id // TODO: This used to be "Frames since update" using the cooldown mechanism, ie. update least-recently-updated worker
+        worker.id.toDouble // TODO: This used to be "Frames since update" using the cooldown mechanism, ie. update least-recently-updated worker
       })
 
     // Update workers in priority order
-    // TODO: Cap worker updates ber run, for performance
+    // TODO: Cap worker updates per run, for performance
     // TODO: Respect cooldown, if needed
     workersToUpdate
       .view
@@ -167,8 +170,7 @@ class Gather extends Plan {
       })
       .foreach(worker => {
         //TODO: Update frames since update
-        val resourceBefore = resourceByWorker.get(worker);
-        val resourceBeforeEdgePixels = resourceBefore.map(_.pixelDistanceEdge(worker))
+        val resourceBefore = resourceByWorker.get(worker)
         unassignWorker(worker)
         if (resourceBefore.exists(_.unitClass.isGas)) {
           gasWorkersNow -= 1
@@ -176,112 +178,87 @@ class Gather extends Plan {
 
         val gasWorkerDesire = if (gasWorkersNow < gasWorkersMax) 1.0 else 0.1
 
-        // Evaluate the marginal efficacy of assigning this worker to a resource.
-        def scoreResource(resource: UnitInfo): Double = {
-          val kInvalid = 1e100d
-          // How many workers are already mining this patch?
-          // TODO: Originally had the unimplemented comment "Count only close-or-closer workers so new miners don't scare us off."
-          val workersBefore = workersByResource.get(resource).map(_.size).getOrElse(0)
-
-          // How effective will the next worker be on this resource?
-          var throughput = 0.001
-          if (resource.unitClass.isGas) {
-            // Depends on distance, but generally a geyser only supports three workers
-            if (workersBefore == 3) {
-              throughput = 0.01
-            } else if (workersBefore < 3) {
-              throughput = 1.0
-            }
-            // Account for geyser depletion
-            if (resource.resourcesLeft < 8) {
-              throughput *= 0.25
-            }
-          } else {
-            if (workersBefore == 0) {
-              throughput = 1.0
-            } else if (workersBefore == 1) {
-              throughput = 0.8
-            } else if (workersBefore == 2) {
-              throughput = 0.2
-            }
-          }
-
-          // How fast is mining from this resource?
-          val depotToResourceDistance = depotToResourceDistances(resource)
-          val speed = 3.0 + 12.0 / Math.max(12.0, depotToResourceDistance)
-
-          if (depotToResourceDistance > kDistanceMining) {
-            if ( ! canDistanceMine) {
-              return kInvalid
-            }
-          }
-
-          // When deciding whether to travel to another base to mine, there's a
-          // tradeoff between mining efficiency and the time-discounted value of
-          // resources.
-          // There's no obvious way to measure the tradeoff, so it's left as a
-          // hyperparameter.
-          val stick = resourceBefore.contains(resource) &&
-              ! worker.carryingResources &&
-              resourceBeforeEdgePixels.forall(_ < 8)
-          val baseFrom = resourceBefore.flatMap(_.base)
-          val baseTo = resource.base
-          val workerToResource = baseFrom.flatMap(baseA => baseTo.map(baseB => baseCosts(baseA, baseB))).getOrElse(worker.pixelDistanceEdge(resource))
-          val kHysteresisPixels = 12
-          val kLookaheadFrames = 24 * 60
-          val workerToResourceHysteresis = if (stick) 0 else 12
-          val framesToResource = worker.framesToTravelPixels(workerToResource + workerToResourceHysteresis)
-          val framesSpentGathering = Math.max(24, kLookaheadFrames - framesToResource)
-          val gasPreference = if (resource.unitClass.isGas) gasWorkerDesire else 1.0
-          val stickiness = if (stick) 2.0 else 1.0
-          val output = -throughput * speed * framesSpentGathering * gasPreference * stickiness
-          output
-        }
-        /*
-
-
-
-
-          // When deciding whether to travel to another base to mine, there's a
-          // tradeoff between mining efficiency and the time-discounted value of
-          // resources.
-          // There's no obvious way to measure the tradeoff, so it's left as a
-          // hyperparameter.
-          const bool stick = resourceBefore == resource &&
-              !worker->carryingResources() &&
-              (resourceBefore == nullptr || resourceBeforeEdgePixels > 1);
-          const auto baseFrom = utils::contains(resourceBases, resourceBefore)
-              ? resourceBases[resourceBefore]
-              : nullptr;
-          const auto baseTo = utils::contains(resourceBases, resource)
-              ? resourceBases[resource]
-              : nullptr;
-          const double workerToResource = (baseFrom && baseTo && baseFrom != baseTo)
-              ? baseCosts[baseFrom][baseTo]
-              : utils::distanceBB(worker, resource);
-          const double framesToResource =
-              (workerToResource + (stick ? 0.0 : FLAGS_gatherer_sticky_distance)) /
-              std::max(.01, worker->topSpeed);
-          const double framesGathering =
-              std::max(24.0, FLAGS_gatherer_lookahead - framesToResource);
-          const double preference = resource->type->isGas ? gasWorkerDesire : 1.0;
-          const double stickiness = stick ? FLAGS_gatherer_sticky_multiplier : 1.0;
-
-          VLOG(6) << fmt::format(
-              "Eval of {} for {}: {} {} {} {} {}",
-              utils::unitString(resource),
-              utils::unitString(worker),
-              throughput,
-              speed,
-              int(framesGathering),
-              preference,
-              stickiness);
-          return -throughput * speed * framesGathering * preference * stickiness;
-             */
+        // Assign the worker to tbhe best resource
+        val resourceBest = ByOption.maxBy(workersByResource.keysIterator)(scoreResource(worker, _, gasWorkerDesire))
+        resourceBest.foreach(bestResource => {
+          assignWorker(worker, bestResource)
+          if (resourceBefore.exists(_.unitClass.isGas)) gasWorkersNow -= 1
+          if (bestResource.unitClass.isGas) gasWorkersNow += 1
+        })
       })
+
+    workersToUpdate.foreach(worker => worker.agent.intend(this, new Intention {
+      toGather = resourceByWorker.get(worker)
+    }))
   }
 
+  // Evaluate the marginal efficacy of assigning this worker to a resource.
+  def scoreResource(worker: FriendlyUnitInfo, resource: UnitInfo, gasWorkerDesire: Double): Double = {
+    val kInvalid = 1e100d
 
+    val resourceBefore = resourceByWorker.get(worker);
+    val resourceBeforeEdgePixels = resourceBefore.map(_.pixelDistanceEdge(worker))
 
+    // How many workers are already mining this patch?
+    // TODO: Originally had the unimplemented comment "Count only close-or-closer workers so new miners don't scare us off."
+    val workersBefore = workersByResource.get(resource).map(_.size).getOrElse(0)
 
+    // How effective will the next worker be on this resource?
+    var throughput = 0.001
+    if (resource.unitClass.isGas) {
+      // Depends on distance, but generally a geyser only supports three workers
+      if (workersBefore == 3) {
+        throughput = 0.01
+      } else if (workersBefore < 3) {
+        throughput = 1.0
+      }
+      // Account for geyser depletion
+      if (resource.resourcesLeft < 8) {
+        throughput *= 0.25
+      }
+    } else {
+      if (workersBefore == 0) {
+        throughput = 1.0
+      } else if (workersBefore == 1) {
+        throughput = 0.8
+      } else if (workersBefore == 2) {
+        throughput = 0.2
+      }
+    }
+
+    // How fast is mining from this resource?
+    val depotToResourceDistance = depotToResourceDistances()(resource)
+    val speed = 3.0 + 12.0 / Math.max(12.0, depotToResourceDistance)
+
+    /*
+    TODO: Reenable
+    if (depotToResourceDistance > kDistanceMining) {
+      if ( ! canDistanceMine) {
+        return kInvalid
+      }
+    }
+
+     */
+
+    // When deciding whether to travel to another base to mine, there's a
+    // tradeoff between mining efficiency and the time-discounted value of
+    // resources.
+    // There's no obvious way to measure the tradeoff, so it's left as a
+    // hyperparameter.
+    val stick = resourceBefore.contains(resource) &&
+        ! worker.carryingResources &&
+        resourceBeforeEdgePixels.forall(_ < 8)
+    val baseFrom = worker.base.orElse(resourceBefore.flatMap(_.base))
+    val baseTo = resource.base
+    val workerToResourceCost = baseFrom.flatMap(baseA => baseTo.map(baseB => baseCosts(baseA, baseB)())).getOrElse(worker.pixelDistanceTravelling(resource.pixelCenter))
+    val kHysteresisPixels = 12
+    val kLookaheadFrames = 24 * 60
+    val workerToResourceHysteresis = if (stick) 0 else 12
+    val framesToResource = worker.framesToTravelPixels(workerToResourceCost + workerToResourceHysteresis)
+    val framesSpentGathering = Math.max(24, kLookaheadFrames - framesToResource)
+    val gasPreference = if (resource.unitClass.isGas) gasWorkerDesire else 1.0
+    val stickiness = if (stick) 2.0 else 1.0
+    val output = throughput * speed * framesSpentGathering * gasPreference * stickiness
+    output
+  }
 }

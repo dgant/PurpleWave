2ff9334231ab9c5a072cdc922e9b715b51889b71
Dan Gant <dgant@users.noreply.github.com>
2020-01-15 22:49:14 -0500

    [WIP] Implemented skeleton of new squad assignment. Last green refactor for a while.

---
+++ src/Micro/Squads/SquadAssignment.scala
@@ -0,0 +1,15 @@
+package Micro.Squads
+
+import ProxyBwapi.UnitInfo.FriendlyUnitInfo
+
+import scala.collection.mutable.ArrayBuffer
+
+class SquadAssignment(squad: Squad) {
+  private val assignedUnits = new ArrayBuffer[FriendlyUnitInfo]
+
+  def units: Seq[FriendlyUnitInfo] = assignedUnits
+
+  def addUnit(unit: FriendlyUnitInfo): Unit = {
+    assignedUnits += unit
+  }
+}
--- src/Micro/Squads/SquadBatch.scala
+++ src/Micro/Squads/SquadBatch.scala
@@ -1,21 +1,59 @@
 package Micro.Squads
 
+import Lifecycle.With
 import ProxyBwapi.UnitInfo.FriendlyUnitInfo
 
 import scala.collection.mutable
 
 class SquadBatch {
 
+  // TODO: Wrap these; don't try to access them externally
   val squads = new mutable.ArrayBuffer[Squad]
   val freelancers = new mutable.ArrayBuffer[FriendlyUnitInfo]
-
-  private var started: Boolean = false
-  private var finished: Boolean = false
 
   def processingStarted: Boolean = started
   def processingFinished: Boolean = finished
 
+  private var started: Boolean = false
+  private var finished: Boolean = false
+
+  private var eligibleSquads = new mutable.ListBuffer[SquadAssignment]
+  private var freelancersUnassigned = new mutable.Queue[FriendlyUnitInfo]
+
   def step(): Unit = {
-    started = true
+    if ( ! started) {
+      eligibleSquads ++= squads.map(new SquadAssignment(_))
+      freelancersUnassigned ++= freelancers
+      started = true
+
+      // TODO: Sort the freelancers by max(Proximity*SquadValue*max(QualityNeed))
+      freelancersUnassigned.sortBy(f => f.id)
+    }
+
+    if (freelancersUnassigned.isEmpty) {
+      val incompleteSquad = eligibleSquads.reverseIterator.find(squad => false) // TODO: Disqualify incomplete squads and release their units before finishing
+      if (incompleteSquad.isDefined) {
+        freelancersUnassigned ++= incompleteSquad.get.units
+        eligibleSquads -= incompleteSquad.get
+        return
+      }
+
+      finished = true
+      return
+    }
+
+    if (eligibleSquads.isEmpty) {
+      With.logger.warn("Ran out of eligible squads with " + freelancersUnassigned.length + " unassigned freelancers")
+      finished = true
+      return
+    }
+
+    // Assign the front freelancer
+    val freelancer = freelancersUnassigned.dequeue()
+    eligibleSquads.maxBy(squadValue(freelancer, _)).addUnit(freelancer)
+  }
+
+  def squadValue(freelancer: FriendlyUnitInfo, squad: SquadAssignment): Double = {
+    1.0 // TODO: Calculate max(Proximity*SquadValue*max(QualityNeed))
   }
 }

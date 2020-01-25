19312660b4b1b216783d60b6391e979a5d3a1ec1
Dan Gant <dgant@users.noreply.github.com>
2020-01-25 16:41:18 -0500

    TorchUp RC7

--- src/Strategery/Playbook.scala
+++ src/Strategery/Playbook.scala
@@ -74,4 +74,4 @@ class TestingPlaybook extends PurpleWavePlaybook {
   override def respectHistory: Boolean = false
 }
 
-object Playbook extends TestingPlaybook {}
+object Playbook extends TournamentPlaybook {}

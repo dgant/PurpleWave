0d34222e54b0c993735c7b1e9ebbeea60ded1419
Dan Gant <dgant@users.noreply.github.com>
2020-07-01 17:50:58 -0400

    Fixed merge error in FollowBuildOrder

--- src/Planning/Plans/Macro/BuildOrders/FollowBuildOrder.scala
+++ src/Planning/Plans/Macro/BuildOrders/FollowBuildOrder.scala
@@ -4,7 +4,11 @@ import Lifecycle.With
 import Planning.Plan
 
 class FollowBuildOrder extends Plan {
+
+  override def getChildren: Iterable[Plan] = With.buildPlans.getChildren
+
   override def onUpdate() {
     With.buildPlans.update(this)
+    getChildren.foreach(delegate)
   }
 }

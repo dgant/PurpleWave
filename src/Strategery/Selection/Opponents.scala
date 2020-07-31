cda6d298f6a294e2cf532db9b5710e236912ec03
Dan Gant <dgant@users.noreply.github.com>
2020-07-31 01:10:13 -0400

    Updated JBWAPI version. Fixed bug in identifying unit visibility to enemies. Improved selection of defense locations for bases with non-entrance wall. Added initial COG opponent configuration.

--- src/Strategery/Selection/Opponents.scala
+++ src/Strategery/Selection/Opponents.scala
@@ -1,14 +1,24 @@
 package Strategery.Selection
 
+import Strategery.Strategies.Protoss._
+
 object Opponents {
   private var allKnown: Vector[Opponent] = Vector.empty
   private def add(name: String, policy: StrategySelectionPolicy = StrategySelectionGreedy()): Opponent = { val output = Opponent(name, policy); allKnown = allKnown :+ output; output }
+
+  // COG 2020
+  val microwave   : Opponent = add("Microwave")
+  val bananabrain : Opponent = add("BananaBrain")
+  val zzzkbot     : Opponent = add("ZZZKBot",   new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ1BaseForgeTech, PvZMidgameNeoBisu, PvZLateGameTemplar))
+  val metabot     : Opponent = add("MetaBot",   StrategySelectionFixed(PvPRobo))
+  val betastar    : Opponent = add("BetaStar")
+  val xiaoyi      : Opponent = add("XIAOYI",    StrategySelectionFixed(PvT13Nexus, PvT2BaseCarrier))
 
   // Aliased
   val iron        : Opponent = add("Iron")
   val letabot     : Opponent = add("LetaBot")
   val megabot     : Opponent = add("MegaBot")
-  val zzzkbot     : Opponent = add("ZZZKBot")
+  //val zzzkbot     : Opponent = add("ZZZKBot")
   val ualbertabot : Opponent = add("UAlbertaBot")
   val aiur        : Opponent = add("AIUR")
   val overkill    : Opponent = add("Overkill")

6df368e2e87e29e734bb6b34d560cffebc64de82
Dan Gant <dgant@users.noreply.github.com>
2020-09-12 17:55:31 -0400

    More AIIDE 2020 strategy population

--- src/Strategery/Selection/Opponents.scala
+++ src/Strategery/Selection/Opponents.scala
@@ -6,29 +6,34 @@ object Opponents {
   private var allKnown: Vector[Opponent] = Vector.empty
   private def add(name: String, policy: StrategySelectionPolicy = StrategySelectionGreedy()): Opponent = { val output = Opponent(name, policy); allKnown = allKnown :+ output; output }
 
+  val defaultPvT = new StrategySelectionRecommended(StrategySelectionGreedy(), PvT32Nexus, PvT2BaseReaver, PvT3BaseArbiter)
+  val defaultPvP = new StrategySelectionRecommended(StrategySelectionGreedy(), PvPRobo, PvP3rdBaseSlow)
+  val defaultPvZ = new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ2Gate910, PvZ4GateGoon, PvZMidgame5GateGoonReaver, PvZLateGameTemplar)
+
   // AIIDE 2020
   val bananabrain : Opponent = add("BananaBrain")
-  val dandanbot   : Opponent = add("DanDanBot")
+  val dandanbot   : Opponent = add("DanDanBot",   defaultPvP)
   val dragon      : Opponent = add("Dragon",      new StrategySelectionRecommended(StrategySelectionGreedy(), PvT13Nexus, PvT2BaseReaver, PvT3BaseGateway))
   val ecgberht    : Opponent = add("Ecgberht",    new StrategySelectionRecommended(StrategySelectionGreedy(), PvT32Nexus, PvT2BaseReaver, PvT3BaseGateway))
-  val eggbot      : Opponent = add("Eggbot")
+  val eggbot      : Opponent = add("Eggbot",      defaultPvP)
   val mcrave      : Opponent = add("McRave",      new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ2Gate1012, PvZ4GateGoon, PvZMidgame5GateGoonReaver, PvZLateGameReaver))
   val microwave   : Opponent = add("Microwave",   new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ2Gate1012, PvZ4GateGoon, PvZMidgame5GateGoonReaver, PvZLateGameReaver))
-  val randofoo    : Opponent = add("Randofoo")
-  val steamhammer : Opponent = add("Steamhammer", new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ2Gate1012, PvZ4GateGoon, PvZMidgame5GateGoonReaver, PvZLateGameReaver))
-  val taij        : Opponent = add("Taij")
+  val randofoo    : Opponent = add("Randofoo",    defaultPvP)
+  val steamhammer : Opponent = add("Steamhammer", new StrategySelectionRecommended(StrategySelectionDynamic(), PvZ2Gate1012, PvZ4GateGoon, PvZMidgame5GateGoonReaver, PvZLateGameReaver))
+  val stardust    : Opponent = add("Stardust")
+  val taij        : Opponent = add("Taij",        defaultPvT)
   val willyt      : Opponent = add("WillyT",      new StrategySelectionRecommended(StrategySelectionGreedy(), PvT28Nexus, PvT2BaseReaver, PvT3BaseArbiter))
   val zzzkbot     : Opponent = add("ZZZKBot",     new StrategySelectionRecommended(StrategySelectionGreedy(), PvZ1BaseForgeTech, PvZMidgameNeoBisu, PvZLateGameTemplar))
   val daqin       : Opponent = add("DaQin",       StrategySelectionFixed(PvP2GateDTExpand, PvP3rdBaseSlow))
   val ualbertabot : Opponent = add("UAlbertaBot", StrategySelectionFixed(PvR1BaseDT))
 
   // COG 2020
-  val metabot     : Opponent = add("MetaBot",   StrategySelectionFixed(PvPRobo))
+  val metabot     : Opponent = add("MetaBot",     StrategySelectionFixed(PvPRobo))
   val betastar    : Opponent = add("BetaStar")
-  val xiaoyi      : Opponent = add("XIAOYI",    StrategySelectionFixed(PvT13Nexus, PvT2BaseCarrier))
-  val aiur        : Opponent = add("AIUR",      StrategySelectionFixed(PvPRobo)) // Metabot stand-in
-  val skynet      : Opponent = add("Skynet",    StrategySelectionFixed(PvPRobo)) // Metabot stand-in
-  val ximp        : Opponent = add("XIMP",      StrategySelectionFixed(PvPRobo)) // Metabot stand-in
+  val xiaoyi      : Opponent = add("XIAOYI",      StrategySelectionFixed(PvT13Nexus, PvT2BaseCarrier))
+  val aiur        : Opponent = add("AIUR",        StrategySelectionFixed(PvPRobo)) // Metabot stand-in
+  val skynet      : Opponent = add("Skynet",      StrategySelectionFixed(PvPRobo)) // Metabot stand-in
+  val ximp        : Opponent = add("XIMP",        StrategySelectionFixed(PvPRobo)) // Metabot stand-in
 
   // Aliased
   val iron        : Opponent = add("Iron")

2bffe64fbca02b40d060ba7c4ea8eed6817473a0
Dan Gant <dgant@users.noreply.github.com>
2020-09-26 01:06:44 -0400

    Added missing file. Updated JBWAPI (cosmetic change)

---
+++ src/Information/Battles/ProcessingStates/BattleProcessMatchupAnalysis.scala
@@ -0,0 +1,24 @@
+package Information.Battles.ProcessingStates
+
+import Lifecycle.With
+import ProxyBwapi.UnitInfo.UnitInfo
+
+import scala.collection.mutable
+
+class BattleProcessMatchupAnalysis extends BattleProcessState {
+
+  lazy val units = new mutable.Queue[UnitInfo]
+  units ++= With.battles.nextBattlesLocal.view.flatMap(_.teams.view.flatMap(_.units))
+
+  override def step(): Unit = {
+    if (units.isEmpty) {
+      transitionTo(new BattleProcessComplete)
+      return
+    }
+
+    val nextUnit = units.dequeue()
+    nextUnit.matchups.threatsInRange
+    nextUnit.matchups.targetsInRange
+    nextUnit.matchups.framesOfSafety
+  }
+}

bc03219bae221cb52a17545f737bd7fef1904177
Dan Gant <dgant@users.noreply.github.com>
2025-07-10 16:41:22 -0400

    Fixed UpgradeContinuously

--- src/Macro/Actions/UpgradeContinuously.scala
+++ src/Macro/Actions/UpgradeContinuously.scala
@@ -1,17 +1,20 @@
 package Macro.Actions
 
 import Lifecycle.With
+import ProxyBwapi.UnitClasses.UnitClasses
 import ProxyBwapi.Upgrades.Upgrade
 
 object UpgradeContinuously extends MacroActions {
 
   def apply(upgrade: Upgrade, maxLevel: Int = 3): Boolean = {
 
-    if (With.self.getUpgradeLevel(upgrade) < Math.min(maxLevel, upgrade.levels.size)
-      && With.units.existsOurs(upgrade.whatUpgrades)
-      && upgrade.whatsRequired.values.forall(With.units.existsOurs(_))) {
+    val nextUpgradeLevel  = With.self.getUpgradeLevel(upgrade) + 1
+    val nextRequirement   = upgrade.whatsRequired(nextUpgradeLevel)
+    val haveUpgrader      = With.units.existsOurs(upgrade.whatUpgrades)
+    val haveRequirement   = nextRequirement == UnitClasses.None || With.units.existsOurs(nextRequirement)
 
-      get(upgrade, With.self.getUpgradeLevel(upgrade) + 1)
+    if (With.self.getUpgradeLevel(upgrade) < Math.min(maxLevel, upgrade.levels.size) && haveUpgrader && haveRequirement) {
+      get(upgrade, nextUpgradeLevel)
     }
 
     upgradeStarted(upgrade, maxLevel)
